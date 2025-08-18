768b61d8014556ab53bbf145c403d7b554d774c8
benjamin <2000beni770@gmail.com>
2025-08-18 09:19:27 +0300

    upload data_lo  file

--- services/data_loader.py
+++ services/data_loader.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from  .mysql_connector import init_db, init_table, select_table
 app = FastAPI()
 
+init_db()
+init_table()
+
 
 @app.get("/")
 def read_root():
-    init_db()
-    init_table()
     res = select_table()
-
     return {"res": res}
 
 
